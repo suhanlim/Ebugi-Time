a58996514b3c5ae37a764287d187ddca88f1041d
suhanlim <soohan200964@gmail.com>
2023-11-14 23:44:36 +0900

    Feat: timeWidget activate
    - useState, setInterval
    - useEffect: no dependency only startPoint

--- src/app/_components/_home/timeWidget.tsx
+++ src/app/_components/_home/timeWidget.tsx
@@ -1,34 +1,49 @@
+"use client";
 import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 
 export function TimeWidget() {
-  const day = dayjs().date();
-  const hour = dayjs().hour();
-  const minute = dayjs().minute();
-  const second = dayjs().second();
+  const [datetime, setDatetime] = useState(dayjs());
 
+  useEffect(() => {
+    const intervalRef = setInterval(() => {
+      setDatetime(dayjs());
+    }, 1000);
+    return () => {
+      clearInterval(intervalRef);
+    };
+  }, []);
   return (
     <div className="grid auto-cols-max grid-flow-col items-center justify-center gap-2 text-center ">
       <div className="rounded-box flex flex-col bg-neutral p-2 text-neutral-content">
         <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": day } as React.CSSProperties}></span>
+          <span
+            style={{ "--value": datetime.day() } as React.CSSProperties}
+          ></span>
         </span>
         days
       </div>
       <div className="rounded-box flex flex-col bg-neutral p-2 text-neutral-content">
         <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": hour } as React.CSSProperties}></span>
+          <span
+            style={{ "--value": datetime.hour() } as React.CSSProperties}
+          ></span>
         </span>
         hours
       </div>
       <div className="rounded-box flex flex-col bg-neutral p-2 text-neutral-content">
         <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": minute } as React.CSSProperties}></span>
+          <span
+            style={{ "--value": datetime.minute() } as React.CSSProperties}
+          ></span>
         </span>
         min
       </div>
       <div className="rounded-box flex flex-col bg-neutral p-2 text-neutral-content">
         <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": second } as React.CSSProperties}></span>
+          <span
+            style={{ "--value": datetime.second() } as React.CSSProperties}
+          ></span>
         </span>
         sec
       </div>
