806fdde1c4f7f872891774c8065eda60d213756b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-14 16:03:34 +0900

    상단 게시판 부분 연결

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,3 +1,10 @@
+
+import { Navigation } from "~/app/_components/_common/navigation"
 export default function Page() {
-  return <p>post 페이지 : 최강</p>;
+  return (
+    // 상단 게시판 부분
+    <Navigation/>
+    // 
+    
+  )
 }
